5957fe92e24cb2799952d1989f392cedea23bcbb
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-12 22:25:50 +0900

    feat(logservice): LogService 클래스 수정
    사용자의 운동 기록 추가 및 유저 아이디를 통한 운동 기록 검색

--- src/main/java/com/healthmanagement/personalized_health_service/service/LogService.java
+++ src/main/java/com/healthmanagement/personalized_health_service/service/LogService.java
@@ -1,6 +1,8 @@
 package com.healthmanagement.personalized_health_service.service;
 
+import com.healthmanagement.personalized_health_service.model.ExerciseLog;
 import com.healthmanagement.personalized_health_service.model.MealLog;
+import com.healthmanagement.personalized_health_service.repository.ExerciseLogRepository;
 import com.healthmanagement.personalized_health_service.repository.MealLogRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,22 @@ public class LogService {
     @Autowired
     private MealLogRepository mealLogRepository;
 
+    @Autowired
+    private ExerciseLogRepository exerciseLogRepository;
+
     public MealLog addMealLog(MealLog mealLog) {
         return mealLogRepository.save(mealLog);
     }
 
+    public ExerciseLog addExerciseLog(ExerciseLog exerciseLog) {
+        return exerciseLogRepository.save(exerciseLog);
+    }
+
     public List<MealLog> getMealLogsByUserId(Long userId) {
         return mealLogRepository.findByUserId(userId);
+    }
+
+    public List<ExerciseLog> getExerciseLogsByUserId(Long userId) {
+        return exerciseLogRepository.findByUserId(userId);
     }
 }
