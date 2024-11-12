2ea366ac3696a3e9825b4ef462fff3dba2a4d8d5
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-12 21:56:41 +0900

    feat(user): UserRepository 클래스 구현

---
+++ src/main/java/com/healthmanagement/personalized_health_service/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.healthmanagement.personalized_health_service.repository;
+
+import com.healthmanagement.personalized_health_service.model.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
