bbd6e451905ce049b07d49962b68303f2179154a
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-13 22:49:30 +0900

    feat(logrepository): logRepository 검색 메서드 추가

--- src/main/java/com/healthmanagement/personalized_health_service/repository/ExerciseLogRepository.java
+++ src/main/java/com/healthmanagement/personalized_health_service/repository/ExerciseLogRepository.java
@@ -1,11 +1,13 @@
 package com.healthmanagement.personalized_health_service.repository;
 
 import com.healthmanagement.personalized_health_service.model.ExerciseLog;
+import com.healthmanagement.personalized_health_service.model.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExerciseLogRepository extends JpaRepository<ExerciseLog, Long> {
-    List<ExerciseLog> findByUserId(Long id);
+    List<ExerciseLog> findTop5ByUserOrderByDateDesc(User user);
+    List<ExerciseLog> findByUserId(Long userId);
 }
--- src/main/java/com/healthmanagement/personalized_health_service/repository/MealLogRepository.java
+++ src/main/java/com/healthmanagement/personalized_health_service/repository/MealLogRepository.java
@@ -1,11 +1,13 @@
 package com.healthmanagement.personalized_health_service.repository;
 
 import com.healthmanagement.personalized_health_service.model.MealLog;
+import com.healthmanagement.personalized_health_service.model.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MealLogRepository extends JpaRepository<MealLog, Long> {
+    List<MealLog> findTop5ByUserOrderByDateDesc(User user);
     List<MealLog> findByUserId(Long userId);
 }
