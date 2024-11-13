165d40ef0407cae0715b56e5209b88f5b21d9017
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-13 22:49:57 +0900

    feat(config): Security 설정 정보 변경

--- src/main/java/com/healthmanagement/personalized_health_service/config/SecurityConfig.java
+++ src/main/java/com/healthmanagement/personalized_health_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,7 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/**").permitAll() // /api/** 경로 허용
                         .anyRequest().authenticated() // 나머지 경로는 인증 요구
