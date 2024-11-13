a4e8553124b3532fc343d8be36d22c4da2a0386b
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-13 18:19:12 +0900

    feat(userservice): 로그인 유효성 검증

--- src/main/java/com/healthmanagement/personalized_health_service/service/UserService.java
+++ src/main/java/com/healthmanagement/personalized_health_service/service/UserService.java
@@ -4,6 +4,7 @@ import com.healthmanagement.personalized_health_service.model.User;
 import com.healthmanagement.personalized_health_service.repository.UserRepository;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,17 @@ public class UserService {
         user.setPassword(encryptedPassword);
 
         return userRepository.save(user);
+    }
+
+    public User loginUser(String email, String password) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("해당 이메일이 존재하지 않습니다."));
+
+        if(!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
     }
 
     public Optional<User> findUserByEmail(String email) {
