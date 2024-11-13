d8f45caa7adab3990c41ee866b6ded4b6c5ba0fd
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-13 18:19:30 +0900

    feat(webconfig): 웹 설정 정보 변경

--- src/main/java/com/healthmanagement/personalized_health_service/config/WebConfig.java
+++ src/main/java/com/healthmanagement/personalized_health_service/config/WebConfig.java
@@ -1,23 +1,18 @@
 package com.healthmanagement.personalized_health_service.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")  // React 프론트엔드 주소
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")  // React 앱의 URL을 명시
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600);
     }
 }
