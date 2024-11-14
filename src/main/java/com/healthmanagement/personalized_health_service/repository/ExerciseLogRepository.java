6b17fbea152f8f3f97a9530993df658eb84beb87
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-14 23:28:09 +0900

    feat(repository): 운동 기록 검색 메서드 추가

--- src/main/java/com/healthmanagement/personalized_health_service/repository/ExerciseLogRepository.java
+++ src/main/java/com/healthmanagement/personalized_health_service/repository/ExerciseLogRepository.java
@@ -2,12 +2,17 @@ package com.healthmanagement.personalized_health_service.repository;
 
 import com.healthmanagement.personalized_health_service.model.ExerciseLog;
 import com.healthmanagement.personalized_health_service.model.User;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ExerciseLogRepository extends JpaRepository<ExerciseLog, Long> {
     List<ExerciseLog> findTop5ByUserOrderByDateDesc(User user);
     List<ExerciseLog> findByUserId(Long userId);
+    List<ExerciseLog> findByUserIdAndDate(Long userId, LocalDate date);
+    @Query("SELECT e FROM ExerciseLog e WHERE e.user.id = :userId AND e.date >= :startDate")
+    List<ExerciseLog> findExerciseLogByUserIdAndDateAfter(Long userId, LocalDate startDate);
 }
