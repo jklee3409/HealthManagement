fb0969330b8c342771605fa6a72be0474e1fc916
Jong Kyu Lee <jklee3409@khu.ac.kr>
2024-11-19 19:38:48 +0900

    Heroku

---
+++ src/main/java/com/healthmanagement/personalized_health_service/HealthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.healthmanagement.personalized_health_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthServiceApplication.class, args);
+	}
+
+}
